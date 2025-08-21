224b866198a8fb8d12db4f0005b61418de6832b6
ctasbihas <ctasbihas@gmail.com>
2025-08-22 04:42:05 +0600

    feat: integrate Redux for state management with axios setup and custom hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router";
 import { Toaster } from "./components/ui/sonner.tsx";
 import "./index.css";
 import { ThemeProvider } from "./providers/theme.provider.tsx";
+import { store } from "./redux/store.ts";
 import { router } from "./routes/index.ts";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<ThemeProvider
-			defaultTheme="system"
-			storageKey="theme"
-		>
-			<RouterProvider router={router} />
-			<Toaster />
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider
+				defaultTheme="system"
+				storageKey="theme"
+			>
+				<RouterProvider router={router} />
+				<Toaster />
+			</ThemeProvider>
+		</Provider>
 	</StrictMode>
 );
