302b91e79a29a0f48d59b5b7d858f5b94d57fb9f
ctasbihas <ctasbihas@gmail.com>
2025-08-27 04:22:53 +0600

    feat: implement Sidebar component with dynamic navigation links and loading skeletons

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,420 +1,35 @@
 import { useUserQuery } from "@/redux/features/auth/auth.api";
-import { Link } from "react-router";
+import { getNavigationLinks } from "@/utils/SidebarItems";
+import { Home } from "lucide-react";
+import { Link, useLocation } from "react-router";
 import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+import { Skeleton } from "../ui/skeleton";
+
+type UserData = {
+	data: {
+		success: boolean;
+		statusCode: number;
+		message: string;
+		data: {
+			_id: string;
+			name: string;
+			email: string;
+			role: string;
+			isBlocked: boolean;
+		};
+	};
+	isLoading: boolean;
+};
 
 const Sidebar = () => {
-	const { data, isSuccess } = useUserQuery(undefined);
-	const isAuthenticated = isSuccess && data.success;
+	const { data, isLoading } = useUserQuery<UserData>(undefined);
+	const { pathname } = useLocation();
 
-	const getNavigationLinks = () => {
-		switch (isAuthenticated && data.data.role) {
-			case "admin":
-				return [
-					{
-						label: "Overview",
-						path: "/dashboard",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M3 12a9 9 0 1 1 18 0 9 9 0 0 1-18 0Z"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-								<path
-									d="M7 13.5 11 10l3 2 3-2.5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "User Management",
-						path: "/dashboard/users",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<circle
-									cx="9"
-									cy="7"
-									r="4"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-								<path
-									d="M22 21v-2a4 4 0 0 0-3-3.87"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<path
-									d="M16 3.13a4 4 0 0 1 0 7.75"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Driver Management",
-						path: "/dashboard/drivers",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<circle
-									cx="12"
-									cy="12"
-									r="3"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-								<path
-									d="M12 1v6M12 17v6M4.22 4.22l4.24 4.24M15.54 15.54l4.24 4.24M1 12h6M17 12h6M4.22 19.78l4.24-4.24M15.54 8.46l4.24-4.24"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Ride Management",
-						path: "/dashboard/rides",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M3 13h18M5 13l2.5-5a3 3 0 0 1 2.7-1.8H14a3 3 0 0 1 2.7 1.8L19 13"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<path
-									d="M6 17h1M17 17h1"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Profile",
-						path: "/dashboard/profile",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<circle
-									cx="12"
-									cy="7"
-									r="4"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-							</svg>
-						),
-					},
-				];
-
-			case "driver":
-				return [
-					{
-						label: "Overview",
-						path: "/dashboard",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M3 12a9 9 0 1 1 18 0 9 9 0 0 1-18 0Z"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-								<path
-									d="M7 13.5 11 10l3 2 3-2.5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Available Rides",
-						path: "/dashboard/available-rides",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M12 2L2 7l10 5 10-5-10-5z"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<path
-									d="M2 17l10 5 10-5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<path
-									d="M2 12l10 5 10-5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Active Ride",
-						path: "/dashboard/active-ride",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M3 13h18M5 13l2.5-5a3 3 0 0 1 2.7-1.8H14a3 3 0 0 1 2.7 1.8L19 13"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<circle
-									cx="6.5"
-									cy="17"
-									r="1.5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-								<circle
-									cx="17.5"
-									cy="17"
-									r="1.5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Earnings",
-						path: "/dashboard/earnings",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Ride History",
-						path: "/dashboard/ride-history",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M4 7h16M4 12h16M4 17h10"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Profile",
-						path: "/dashboard/profile",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<circle
-									cx="12"
-									cy="7"
-									r="4"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-							</svg>
-						),
-					},
-				];
-
-			case "rider":
-			default:
-				return [
-					{
-						label: "Profile",
-						path: "/dashboard",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M3 12a9 9 0 1 1 18 0 9 9 0 0 1-18 0Z"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-								<path
-									d="M7 13.5 11 10l3 2 3-2.5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Book A Ride",
-						path: "/dashboard/book-ride",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									d="M3 13h18M5 13l2.5-5a3 3 0 0 1 2.7-1.8H14a3 3 0 0 1 2.7 1.8L19 13M6 17h1M17 17h1"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Active Ride",
-						path: "/dashboard/active-ride",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M3 13h18M5 13l2.5-5a3 3 0 0 1 2.7-1.8H14a3 3 0 0 1 2.7 1.8L19 13"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<circle
-									cx="6.5"
-									cy="17"
-									r="1.5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-								<circle
-									cx="17.5"
-									cy="17"
-									r="1.5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-								/>
-							</svg>
-						),
-					},
-					{
-						label: "Ride History",
-						path: "/dashboard/ride-history",
-						icon: (
-							<svg
-								className="h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M4 7h16M4 12h16M4 17h10"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-								/>
-							</svg>
-						),
-					},
-				];
-		}
-	};
-
-	const navigationLinks = getNavigationLinks();
+	const navigationLinks = getNavigationLinks(data?.data?.role || "");
 
 	return (
-		<aside className="flex flex-col min-h-screen w-72 border-r p-4 bg-sidebar text-sidebar-foreground border-sidebar-border">
+		<aside className="flex flex-col min-h-screen min-w-60 border-r p-4 bg-sidebar text-sidebar-foreground border-sidebar-border">
 			<div className="mb-6 flex items-center gap-3">
 				<div>
 					<h2 className="text-2xl font-semibold">ZipRide</h2>
@@ -424,77 +39,84 @@ const Sidebar = () => {
 
 			<nav>
 				<ul className="mb-4 space-y-1">
-					{navigationLinks.map((link, index) => (
-						<li key={link.path}>
-							<Link
-								to={link.path}
-								className={`group relative flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium hover:bg-sidebar-accent ${
-									index === 0 ? "bg-sidebar-accent" : ""
-								}`}
-							>
-								{index === 0 && (
-									<span className="absolute left-0 top-1/2 h-8 w-[3px] -translate-y-1/2 rounded-full bg-sidebar-foreground" />
-								)}
-								<span
-									className={`${
-										index === 0
-											? ""
-											: "text-muted-foreground group-hover:text-sidebar-foreground"
+					{!isLoading ? (
+						navigationLinks.map((link) => (
+							<li key={link.path}>
+								<Link
+									to={link.path}
+									className={`group relative flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium hover:bg-sidebar-accent ${
+										pathname === link.path
+											? "bg-sidebar-accent"
+											: ""
 									}`}
 								>
-									{link.icon}
-								</span>
-								<span>{link.label}</span>
-							</Link>
-						</li>
-					))}
+									{pathname === link.path && (
+										<span className="absolute left-0 top-1/2 h-8 w-[3px] -translate-y-1/2 rounded-full bg-sidebar-foreground" />
+									)}
+									<span
+										className={`${
+											pathname === link.path
+												? ""
+												: "text-muted-foreground group-hover:text-sidebar-foreground"
+										}`}
+									>
+										{link.icon}
+									</span>
+									<span>{link.label}</span>
+								</Link>
+							</li>
+						))
+					) : (
+						<>
+							{[...Array(4)].map((_, i) => (
+								<li key={i}>
+									<div className="flex items-center gap-3 rounded-lg px-3 py-2">
+										<Skeleton className="h-7 w-7 rounded-xl" />
+										<Skeleton className="h-7 w-full rounded-xl" />
+									</div>
+								</li>
+							))}
+						</>
+					)}
+					<Separator className="my-2" />
+					<Button
+						asChild
+						variant={"ghost"}
+						className="w-full justify-start"
+					>
+						<Link to="/">
+							<Home className="h-4 w-4" /> Home
+						</Link>
+					</Button>
 				</ul>
 			</nav>
 
 			<div className="mt-auto rounded-lg border p-3 bg-sidebar-accent border-sidebar-border">
 				<div className="flex items-center gap-3">
-					<span className="grid h-9 w-9 place-items-center rounded-full bg-sidebar-foreground text-2xl font-bold text-sidebar">
-						{isAuthenticated
-							? data.data.name.charAt(0).toUpperCase()
-							: ""}
-					</span>
+					{isLoading ? (
+						<Skeleton className="h-9 w-9 rounded-full bg-sidebar-foreground" />
+					) : (
+						<span className="grid h-9 w-9 place-items-center rounded-full bg-sidebar-foreground text-2xl font-bold text-sidebar">
+							{data.data.name.charAt(0).toUpperCase()}
+						</span>
+					)}
 					<div className="min-w-0 flex-1">
-						<h3 className="truncate text-sm font-medium">
-							{isAuthenticated ? data.data.name : ""}
-						</h3>
-						<h4 className="truncate text-xs text-muted-foreground">
-							{isAuthenticated ? data.data.email : ""}
-						</h4>
+						{isLoading ? (
+							<>
+								<Skeleton className="h-4 w-full bg-sidebar mb-1 rounded" />
+								<Skeleton className="h-3 w-full bg-sidebar rounded" />
+							</>
+						) : (
+							<>
+								<h3 className="truncate text-sm font-medium">
+									{data.data.name}
+								</h3>
+								<h4 className="truncate text-xs text-muted-foreground">
+									{data.data.email}
+								</h4>
+							</>
+						)}
 					</div>
-
-					<Button
-						asChild
-						variant="outline"
-						className="gap-2"
-					>
-						<Link to="/">
-							<svg
-								className="h-4 w-4"
-								viewBox="0 0 24 24"
-								fill="none"
-							>
-								<path
-									d="M3 10.5 12 4l9 6.5"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-								<path
-									d="M5 10v10h14V10"
-									stroke="currentColor"
-									strokeWidth="1.6"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
-						</Link>
-					</Button>
 				</div>
 			</div>
 		</aside>
---
+++ src/utils/SidebarItems.tsx
@@ -0,0 +1,111 @@
+import {
+	BookOpen,
+	Car,
+	DollarSign,
+	History,
+	LayoutDashboard,
+	List,
+	Settings,
+	User,
+	UserCircle2,
+	Users,
+} from "lucide-react";
+
+export const getNavigationLinks = (role: string) => {
+	switch (role) {
+		case "admin":
+			return [
+				{
+					label: "Overview",
+					path: "/dashboard",
+					icon: <LayoutDashboard className="h-5 w-5" />,
+				},
+				{
+					label: "User Management",
+					path: "/dashboard/users",
+					icon: <Users className="h-5 w-5" />,
+				},
+				{
+					label: "Ride Management",
+					path: "/dashboard/rides",
+					icon: <Car className="h-5 w-5" />,
+				},
+				{
+					label: "Profile",
+					path: "/dashboard/profile",
+					icon: <User className="h-5 w-5" />,
+				},
+			];
+
+		case "driver":
+			return [
+				{
+					label: "Overview",
+					path: "/dashboard",
+					icon: <LayoutDashboard className="h-5 w-5" />,
+				},
+				{
+					label: "Available Rides",
+					path: "/dashboard/available-rides",
+					icon: <BookOpen className="h-5 w-5" />,
+				},
+				{
+					label: "Active Ride",
+					path: "/dashboard/active-ride",
+					icon: <Car className="h-5 w-5" />,
+				},
+				{
+					label: "Earnings",
+					path: "/dashboard/earnings",
+					icon: <DollarSign className="h-5 w-5" />,
+				},
+				{
+					label: "Ride History",
+					path: "/dashboard/ride-history",
+					icon: <History className="h-5 w-5" />,
+				},
+				{
+					label: "Profile",
+					path: "/dashboard/profile",
+					icon: <User className="h-5 w-5" />,
+				},
+			];
+
+		case "rider":
+			return [
+				{
+					label: "Dashboard",
+					path: "/dashboard",
+					icon: <UserCircle2 className="h-5 w-5" />,
+				},
+				{
+					label: "Book A Ride",
+					path: "/dashboard/book-ride",
+					icon: <BookOpen className="h-5 w-5" />,
+				},
+				{
+					label: "Active Ride",
+					path: "/dashboard/active-ride",
+					icon: <Car className="h-5 w-5" />,
+				},
+				{
+					label: "Ride History",
+					path: "/dashboard/ride-history",
+					icon: <List className="h-5 w-5" />,
+				},
+				{
+					label: "Profile",
+					path: "/dashboard/profile",
+					icon: <UserCircle2 className="h-5 w-5" />,
+				},
+				{
+					label: "Settings",
+					path: "/dashboard/settings",
+					icon: <Settings className="h-5 w-5" />,
+				},
+			];
+
+		default:
+			return [];
+	}
+};
